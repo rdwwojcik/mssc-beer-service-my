901e3b8ace8b7d8a490a009e93c62656517da1ea
radek <rdwwojcik@gmail.com>
2023-06-05 15:27:45 +0200

    init

---
+++ src/main/java/com/softlabs/msscbeerservicemy/MsscBeerServiceMyApplication.java
@@ -0,0 +1,13 @@
+package com.softlabs.msscbeerservicemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceMyApplication.class, args);
+	}
+
+}
