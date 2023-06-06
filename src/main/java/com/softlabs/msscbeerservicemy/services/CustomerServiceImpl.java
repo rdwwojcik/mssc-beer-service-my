a307e509b8cd1fd7908381dfbd0b107def691f19
radek <rdwwojcik@gmail.com>
2023-06-06 13:24:32 +0200

    Adding beer entity

--- src/main/java/com/softlabs/msscbeerservicemy/services/CustomerServiceImpl.java
+++ src/main/java/com/softlabs/msscbeerservicemy/services/CustomerServiceImpl.java
@@ -27,7 +27,7 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Override
     public void updateCustomer(UUID customerId, CustomerDto customerDto) {
-      log.debug("Updating....");
+        log.debug("Updating....");
     }
 
     @Override
--- src/main/java/com/softlabs/msscbeerservicemy/web/controller/CustomerController.java
+++ src/main/java/com/softlabs/msscbeerservicemy/web/controller/CustomerController.java
@@ -19,7 +19,6 @@ public class CustomerController {
 
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId")  UUID customerId){
-
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
 
