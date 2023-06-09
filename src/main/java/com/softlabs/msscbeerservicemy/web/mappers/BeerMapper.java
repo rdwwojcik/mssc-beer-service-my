ed914c05cc517c3878fc5c40e762ceb8a654a777
radek <rdwwojcik@gmail.com>
2023-06-09 14:53:16 +0200

    Adding mapstruct config

---
+++ src/main/java/com/softlabs/msscbeerservicemy/web/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.softlabs.msscbeerservicemy.web.mappers;
+
+import com.softlabs.msscbeerservicemy.domain.Beer;
+import com.softlabs.msscbeerservicemy.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by radek on 2023-06-09
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
