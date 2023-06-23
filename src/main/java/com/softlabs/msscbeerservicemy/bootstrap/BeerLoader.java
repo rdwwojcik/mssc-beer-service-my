12b17209d7cc5a24359deb82dba031559bf592a0
radek <rdwwojcik@gmail.com>
2023-06-23 11:04:27 +0200

    Set H2 dataase, config to create schema and init database

--- src/main/java/com/softlabs/msscbeerservicemy/bootstrap/BeerLoader.java
+++ src/main/java/com/softlabs/msscbeerservicemy/bootstrap/BeerLoader.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class BeerLoader implements CommandLineRunner {
 
